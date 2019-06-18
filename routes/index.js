e1b9572e37db3790732576ec507df221116ce093
ujjwal3012 <46800638+ujjwal3012@users.noreply.github.com>
2019-06-17 20:35:31 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/AboutMe', (req, res, next) => {
+  res.render('AboutMe', { age:20 });
+});
+
+router.get('/Projects', (req, res, next) => {
+  res.render('Projects', {});
+});
+
+router.get('/Services', (req, res, next) => {
+  res.render('Services', {});
+});
+
+router.get('/ContactMe', (req, res, next) => {
+  res.render('ContactMe', {});
 });
 
 module.exports = router;
